1aabb3135f6b1ad27bdb10babc3d4c1aac149b4d
Viet Hoang <viet.hoang@quant-edge.com>
2014-11-28 20:11:11 +0700

    change name to rabbit_auth_backend_ldap_quantedge

--- build/deps.mk
+++
@@ -1,5 +0,0 @@
-$(EBIN_DIR)/rabbit_auth_backend_ldap_quantedge.beam: /home/viethoang/rabbitmq-public-umbrella/rabbitmq-auth-backend-ldap-quantedge/src/rabbit_auth_backend_ldap_quantedge.erl
-$(TEST_EBIN_DIR)/rabbit_auth_backend_ldap_quantedge_test.beam: /home/viethoang/rabbitmq-public-umbrella/rabbitmq-auth-backend-ldap-quantedge/test/src/rabbit_auth_backend_ldap_quantedge_test.erl
-$(EBIN_DIR)/rabbit_auth_backend_ldap_quantedge_util.beam: /home/viethoang/rabbitmq-public-umbrella/rabbitmq-auth-backend-ldap-quantedge/src/rabbit_auth_backend_ldap_quantedge_util.erl
-$(EBIN_DIR)/rabbit_auth_backend_ldap_quantedge_app.beam: /home/viethoang/rabbitmq-public-umbrella/rabbitmq-auth-backend-ldap-quantedge/src/rabbit_auth_backend_ldap_quantedge_app.erl
-$(DEPS_FILE): /home/viethoang/rabbitmq-public-umbrella/generate_deps
